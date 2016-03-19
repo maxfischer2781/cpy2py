b9b51dc37bb6d95d965cb370b76790b91c781891
Max Fischer <max.fischer@kit.edu>
2016-03-19 17:17:08 +0100

    added unittests

---
+++ cpy2cpy_unittests/test_primitives.py
@@ -0,0 +1,42 @@
+import random
+import unittest
+
+import cpy2py.twinterpreter.twin_pypy
+
+
+def add(a, b):
+	return a + b
+
+
+def add_default(a=10, b=20):
+	return a + b
+
+
+class TestFunctionCall(unittest.TestCase):
+	def setUp(self):
+		self.twinterpreter = cpy2py.twinterpreter.twin_pypy.TwinPyPy()
+		self.twinterpreter.start()
+
+	def tearDown(self):
+		self.twinterpreter.stop()
+
+	def _get_test_args(self):
+		return [(1, 1), (50, 100), (500, 250)] + [(random.random(), random.random()) for _ in xrange(5)]
+
+	def test_args(self):
+		for arga, argb in self._get_test_args():
+			self.assertAlmostEqual(add(arga, argb), self.twinterpreter.execute(add, arga, argb))
+
+	def test_kwargs(self):
+		for arga, argb in self._get_test_args():
+			self.assertAlmostEqual(add(arga, argb), self.twinterpreter.execute(add, arga, argb))
+
+	def test_args_defaults(self):
+		for arga, argb in self._get_test_args():
+			self.assertAlmostEqual(add_default(arga), self.twinterpreter.execute(add_default, arga))
+			self.assertAlmostEqual(add_default(argb), self.twinterpreter.execute(add_default, argb))
+
+	def test_kwargs_defaults(self):
+		for arga, argb in self._get_test_args():
+			self.assertAlmostEqual(add_default(a=arga), self.twinterpreter.execute(add_default, a=arga))
+			self.assertAlmostEqual(add_default(b=argb), self.twinterpreter.execute(add_default, b=argb))
