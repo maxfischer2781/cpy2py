7fa93773c661e35a3725baaa1488f9c0fdbb21a5
Max Fischer <maxfischer2781@gmail.com>
2016-03-29 10:34:55 +0200

    ipyc: added support for custom picklers

--- cpy2py/ipyc/stdstream.py
+++ cpy2py/ipyc/stdstream.py
@@ -17,19 +17,31 @@ from cpy2py.ipyc.ipyc_exceptions import IPyCTerminated
 
 
 class StdIPC(object):
-	def __init__(self, readstream=sys.stdin, writestream=sys.stdout):
+	def __init__(
+			self,
+			readstream=sys.stdin,
+			writestream=sys.stdout,
+			pickler_cls=pickle.Pickler,
+			unpickler_cls=pickle.Unpickler,
+			pickle_protocol=pickle.HIGHEST_PROTOCOL
+	):
 		self._readstream = readstream
 		self._writestream = writestream
+		self._pickler_cls = pickler_cls
+		self._unpickler_cls = unpickler_cls
+		self._pkl_protocol = pickle_protocol
+		self._dump = self._pickler_cls(self._writestream, self._pkl_protocol).dump
+		self._load = self._unpickler_cls(self._readstream).load
 
 	def send(self, payload):
 		try:
-			pickle.dump(payload, self._writestream)
+			self._dump(payload)
 			self._writestream.flush()
 		except IOError:
 			raise IPyCTerminated
 
 	def receive(self):
 		try:
-			return pickle.load(self._readstream)
+			return self._load()
 		except EOFError:
 			raise IPyCTerminated
