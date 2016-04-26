8be6dee9200570a870a076002c6b6b3389862a2c
Max Fischer <maxfischer2781@gmail.com>
2016-04-26 15:00:55 +0200

    updated metadata to indicate py2.6 support

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         # TODO: confirm others
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
