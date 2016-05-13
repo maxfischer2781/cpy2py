3a8133ace367af59ddf8e7b493b76b76cf92fc7d
Max Fischer <maxfischer2781@gmail.com>
2016-05-13 07:58:29 +0200

    expanded threadguard method testing

--- cpy2py_unittests/primitives/test_utility__thread_tools.py
+++ cpy2py_unittests/primitives/test_utility__thread_tools.py
@@ -10,13 +10,23 @@ class TestThreadGuard(unittest.TestCase):
     def test_counter(self):
         counter = 1
         tg_counter = ThreadGuard(counter)
-        for op in ['add', 'sub', 'mul', 'floordiv', 'eq', 'ne', 'lt', 'gt', 'le', 'ge', 'and_', 'or_', 'xor']:
+        for op in [
+            '__add__', '__sub__', '__mul__', '__floordiv__', '__pow__', '__mod__',
+            '__eq__', '__ne__', '__lt__', '__gt__', '__le__', '__ge__',
+            '__and__', '__or__', '__xor__',
+            '__lshift__', '__rshift__',
+        ]:
             try:
                 ops = getattr(operator, op)
             except AttributeError:
                 continue
             else:
                 for val in (random.randint(-100, 100) for _ in rangex(5)):
+                    if op in ('__lshift__', '__rshift__'):
+                        val = abs(val)
                     res = ops(counter, val)
                     tg_res = ops(tg_counter, val)
                     self.assertEqual(res, tg_res, '%s %s %s' % (res, tg_res, op))
+                    rres = ops(val, counter)
+                    rtg_res = ops(val, tg_counter)
+                    self.assertEqual(rres, rtg_res, '%s %s %s (reflected)' % (rres, rtg_res, op))
