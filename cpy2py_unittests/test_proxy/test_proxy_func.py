8d570566f80b88350d0014e48c7b9e7025fea98c
Max Fischer <maxfischer2781@gmail.com>
2016-11-19 22:58:24 +0100

    twinfunc meta test

--- cpy2py_unittests/test_proxy/test_proxy_func.py
+++ cpy2py_unittests/test_proxy/test_proxy_func.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 import unittest
 import time
 
@@ -7,11 +8,13 @@ from cpy2py.proxy import proxy_func
 
 @proxy_func.twinfunction(kernel_state.TWIN_ID)
 def native_func(arg):
+    """docstring"""
     return arg
 
 
 @proxy_func.twinfunction('pypy')
 def proxied_func(arg):
+    """docstring"""
     return arg
 
 
@@ -26,10 +29,27 @@ class TestThreadGuard(unittest.TestCase):
 
     def test_call_native(self):
         """Call twinfunction in native scope"""
+        self.assertFalse(hasattr(native_func, '__wrapped__'))
         for arg in ('some arg', [1, 2, 3], {'a': 'A'}):
             self.assertEqual(native_func(arg), arg)
 
     def test_call_proxy(self):
         """Call twinfunction in other scope"""
+        self.assertTrue(hasattr(proxied_func, '__wrapped__'))
         for arg in ('some arg', [1, 2, 3], {'a': 'A'}):
             self.assertEqual(proxied_func(arg), arg)
+
+    def test_meta(self):
+        """Twins share metadata"""
+        for attr in (
+            '__doc__',
+            '__signature__', '__defaults__',
+            '__name__', '__module__',
+            '__qualname__', '__annotations__'
+        ):
+            with self.subTest(attr=attr):
+                if hasattr(native_func, attr):
+                    if attr not in ('__name__', '__qualname__'):
+                        self.assertEqual(getattr(native_func, attr), getattr(proxied_func.__wrapped__, attr))
+                        self.assertEqual(getattr(native_func, attr), getattr(proxied_func, attr))
+                    self.assertEqual(getattr(proxied_func, attr), getattr(proxied_func.__wrapped__, attr))
