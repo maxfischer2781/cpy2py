ca16681ba39df2fb8c30546f46664209800eabd0
Max Fischer <maxfischer2781@gmail.com>
2018-04-23 11:47:55 +0200

    executable discovery test uses unambiguous path testing

--- cpy2py_unittests/test_twinterpreter/test_twin_def.py
+++ cpy2py_unittests/test_twinterpreter/test_twin_def.py
@@ -10,12 +10,12 @@ class TestTwinDef(unittest.TestCase):
         """Initialize with executable only"""
         # from full path
         instance_a = process.TwinProcess(executable=sys.executable)
-        self.assertEqual(sys.executable, instance_a.executable)
+        self.assertEqual(os.path.realpath(sys.executable), instance_a.executable)
         self.assertEqual(os.path.basename(sys.executable), instance_a.twinterpreter_id)
         os_path = os.environ.get('PATH')
         os.environ['PATH'] = os.path.dirname(sys.executable)
         instance_b = process.TwinProcess(executable=os.path.basename(sys.executable))
-        self.assertEqual(sys.executable, instance_b.executable)
+        self.assertEqual(os.path.realpath(sys.executable), instance_b.executable)
         self.assertEqual(os.path.basename(sys.executable), instance_b.twinterpreter_id)
         os.environ['PATH'] = os_path
         self.assertEqual(instance_a, instance_b)
@@ -25,6 +25,6 @@ class TestTwinDef(unittest.TestCase):
         os_path = os.environ.get('PATH')
         os.environ['PATH'] = os.path.dirname(sys.executable)
         instance_b = process.TwinProcess(twinterpreter_id=os.path.basename(sys.executable))
-        self.assertEqual(sys.executable, instance_b.executable)
+        self.assertEqual(os.path.realpath(sys.executable), instance_b.executable)
         self.assertEqual(os.path.basename(sys.executable), instance_b.twinterpreter_id)
         os.environ['PATH'] = os_path
