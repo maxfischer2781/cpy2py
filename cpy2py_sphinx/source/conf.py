5485cc612c868cbc02fdfbaf7be0483457b94101
Max Fischer <maxfischer2781@gmail.com>
2018-05-03 13:30:29 +0200

    changed docs metadata

--- cpy2py_sphinx/source/conf.py
+++ cpy2py_sphinx/source/conf.py
@@ -68,7 +68,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'CPy2Py'
-copyright = u'2016, Max Fischer'
+copyright = u'2016-2018, Max Fischer'
 author = u'Max Fischer'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -131,7 +131,6 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'alabaster'
-html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
